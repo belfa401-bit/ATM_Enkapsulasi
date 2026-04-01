dc06ac47217cddea03ed0047504551e73f095cdf
belfa401-bit <flbelfa401@gmail.com>
2026-04-01 10:01:34 +0700

    Membuat Set up Awal Class Rekening

--- ATM_Enkapsulasi/Program.cs
+++ ATM_Enkapsulasi/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Rekening
+{
+    private string _noRekening;
+
+    private int _saldo;
+
+    public string NoRekening
+    {
+        set
+        {
+            _noRekening = value;
+        }
+    }
+
+    public int Saldo
+    {
+        get {  return _saldo; }
+        set
+        {
+            if (value < 0) Console.WriteLine("Saldo Tidak Boleh Negatif!");
+            else _saldo = value;
+        }
+    }
+
+    public Rekenening (string no_rekening)
+    {
+        NoRekening = no_rekening;
+    }
+
+    public void DisplayInfo()
+    {
+        Console.WriteLine("Menampilkan Info...");
+        Console.WriteLine($"Saldo Saat Ini: {Saldo}");
+    }
+
+}
