ac4fb035420d18636d6654d3dd6e55b4352381ce
belfa401-bit <flbelfa401@gmail.com>
2026-04-08 10:12:09 +0700

    ATM

--- ATM_Enkapsulasi/Program.cs
+++ ATM_Enkapsulasi/Program.cs
@@ -1,4 +1,41 @@
-﻿class Rekening
+﻿Rekening rekening = new Rekening("121212");
+
+bool ulang = true;
+int jumlah;
+string pilihan;
+
+while (ulang)
+{
+    Console.WriteLine("Menu ATM ");
+    Console.WriteLine("1. Display Info \n2. Setor Uang \n3. Tarik Uang \n4. Keluar");
+    Console.WriteLine("INPUT PILIHAN");
+    pilihan = Console.ReadLine();
+
+    switch (pilihan)
+    {
+        case "1":
+            rekening.DisplayInfo();
+            break;
+        case "2":
+            Console.WriteLine("Maukkan Jumlah Uang: Rp ");
+            jumlah = int.Parse(Console.ReadLine());
+            rekening.SetorUang(jumlah);
+            break;
+        case "3":
+            Console.WriteLine("Maukkan Jumlah Uang: Rp ");
+            jumlah = int.Parse(Console.ReadLine());
+            rekening.TarikUang(jumlah);
+            break;
+        case "4":
+            Console.WriteLine("Terimaksih Sudah Menggunakan Layanan Kami!");
+            ulang = false;
+            break;
+    }
+}
+
+
+
+class Rekening
 {
     private string _noRekening;
 
@@ -12,9 +49,9 @@
         }
     }
 
-    public int Saldo
+    public int saldo
     {
-        get {  return _saldo; }
+        get { return _saldo; }
         set
         {
             if (value < 0) Console.WriteLine("Saldo Tidak Boleh Negatif!");
@@ -22,15 +59,50 @@
         }
     }
 
-    public Rekenening (string no_rekening)
+    public string noRekening
     {
-        NoRekening = no_rekening;
+        get { return _noRekening; }
+    }
+
+    public Rekening(string noRekening)
+    {
+        _noRekening = noRekening;
+        _saldo = 100000;
     }
 
     public void DisplayInfo()
     {
         Console.WriteLine("Menampilkan Info...");
-        Console.WriteLine($"Saldo Saat Ini: {Saldo}");
+        Console.WriteLine($"No. Rekening: {noRekening}");
+        Console.WriteLine($"Saldo saat ini: {saldo}");
+    }
+
+    public void SetorUang(int Jumlah)
+    {
+        if (Jumlah < 0)
+        {
+            Console.WriteLine("Jumlah uang tidak boleh negatif");
+        }
+        else
+        {
+            saldo = Jumlah;
+            Console.WriteLine("Berhasil setor uang!");
+            Console.WriteLine($"Saldo saat ini: {saldo}");
+        }
+    }
+
+    public void TarikUang(int Jumlah)
+    {
+        if (Jumlah > saldo)
+        {
+            Console.WriteLine("Saldo kamu tidak mencukupi");
+        }
+        else
+        {
+            saldo = Jumlah;
+            Console.WriteLine("Berhasil tarik uang");
+            Console.WriteLine($"Saldo saaat ini {saldo}");
+        }
     }
 
 }
